20806e3bc4db58e8b3c7c19c12fecaf10a2503a4
Josiah Maharaj <josiahmaharaj@outlook.com>
2020-07-20 11:22:39 -0400

    added broadcasts when user connects and disconnects

--- server.js
+++ server.js
@@ -3,24 +3,28 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello world</h1>');
-  res.sendFile(__dirname + '/pages/index.html');
+    // res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/pages/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
+    //Log new connection
+    console.log('a user connected');
+    //Notify connected users of new connection
+    socket.broadcast.emit('chat message', 'New user connected to Chat');
+    //Notify connected users when someone disconnects
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('chat message', 'User Left');
+        console.log('user disconnected');
+    });
+    //Chat box
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
